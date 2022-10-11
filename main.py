e53371d5d91196d4ab5ab50c84fa1f83b768d7cc
Michel Rivas <michel.rivas@estudiantes.utec.edu.uy>
2022-10-10 22:02:45 -0300

    configuracion cicd ldm api

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import os
 from flask import Flask, jsonify, request
 import load_model
 import prepare_image
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/predict', methods=['POST'])
 def infer_image():
